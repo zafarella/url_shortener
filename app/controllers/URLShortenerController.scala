c5ea9b2f81c2435b23f7d80fda4899ebe80f9728
zafarella <zafar@linux.com>
2017-12-31 00:33:48 -0500

    - another iteration

--- app/controllers/URLShortenerController.scala
+++ app/controllers/URLShortenerController.scala
@@ -5,6 +5,7 @@ import java.util.Random
 import javax.inject._
 
 import akka.actor.ActorSystem
+import io.swagger.annotations.{Api, ApiOperation}
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
@@ -13,6 +14,7 @@ import scala.concurrent.{ExecutionContext, Future, Promise}
   * This is just initial
   */
 @Singleton
+@Api(value = "/shorten", description = "URL shortener")
 class URLShortenerController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext)
   extends AbstractController(cc) {
 
@@ -22,20 +24,26 @@ class URLShortenerController @Inject()(cc: ControllerComponents, actorSystem: Ac
 
   /**
     */
-  def shorten(longURL: String) = Action.async {
-    getFutureShortenedURL(longURL).map { msg => Ok(msg) }
+  @ApiOperation(value = "Shorten url in the POST body.",
+    notes = "",
+    httpMethod = "POST")
+  def shorten = Action.async {
+    implicit request =>
+      getFutureShortenedURL(request.body.asText.getOrElse("")).map { msg => Ok(msg) }
   }
 
   private def getFutureShortenedURL(longUrl: String): Future[String] = {
     val promise: Promise[String] = Promise[String]()
     val r: Random = new Random
-    val shortened: String = baseURL + longUrl.hashCode + String.valueOf(r.nextInt(1000))
+    val shortened: String = baseURL + longUrl.hashCode
     urls.put(shortened, longUrl)
     promise.success(shortened)
     promise.future
   }
 
-
+  @ApiOperation(value = "Return long URL",
+    notes = "",
+    httpMethod = "GET")
   def decode(url: String) = Action.async {
     getFutureDecode(url).map { msg =>
       msg match {
--- build.sbt
+++ build.sbt
@@ -13,8 +13,11 @@ scalaVersion := "2.12.2"
 libraryDependencies ++= Seq(jdbc,
 
   "com.kenshoo" %% "metrics-play" % "2.6.2_0.6.1",
+  "io.swagger" %% "swagger-play2" % "1.6.0" exclude("org.reflections", "reflections"),
+  "org.reflections" % "reflections" % "0.9.8" notTransitive(),
+  "org.webjars" % "swagger-ui" % "2.1.8-M1",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
 
   ehcache, ws, specs2 % Test, guice)
 
-unmanagedResourceDirectories in Test <+= baseDirectory(_ / "target/web/public/test")
 
